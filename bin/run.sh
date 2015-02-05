66ae588d43f98ff4712f527ebaa02b9060efcbe8
omriiluz <omri@iluz.net>
2015-02-04 17:59:51 -0800

    add docker container support

---
+++ bin/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source /root/spark_files/configure_spark.sh
+
+IP=$(ip -o -4 addr list eth0 | perl -n -e 'if (m{inet\s([\d\.]+)\/\d+\s}xms) { print $1 }')
+echo "WORKER_IP=$IP"
+
+echo "preparing Spark"
+prepare_spark $1
+
+source $SPARK_HOME/conf/spark-env.sh
+
+echo "preparing Job Server"
+touch $INSTALL_DIR/settings.sh
+sed -ri s/local\[4\]/$MASTER/g $INSTALL_DIR/server.conf
+
+echo "Run jobserver"
+cd $INSTALL_DIR
+./server_start.sh
+while [ 1 ];
+do
+    tail -f $LOG_DIR/spark-job-server.log
+    sleep 3
+done
